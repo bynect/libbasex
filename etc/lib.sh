a44ad7b9b3057f0cf26930222c3592bd44b6907c
bynect <68197565+bynect@users.noreply.github.com>
2021-01-02 17:13:49 +0100

    Bump license year

--- etc/lib.sh
+++ etc/lib.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-# Copyright 2020 @bynect
+# Copyright 2020-2021 @bynect
 # This is free and open source software.
 # You can use/modify/redistribute it under the terms of the MIT license.
 # See docs/LICENSE for for more details.
--- include/basex/base64.h
+++ include/basex/base64.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 @bynect
+ * Copyright 2020-2021 @bynect
  *
  * This is free and open source software.
  * You can use/modify/redistribute it under the terms of the MIT license.
